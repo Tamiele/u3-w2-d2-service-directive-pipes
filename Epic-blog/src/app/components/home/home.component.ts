3a0ac3300ab8cf5fbabc787f82c32e5bc18222f8
Tamiele <tammaro_85@hotmail.it>
2024-10-15 19:50:41 +0200

    filtro-post

--- Epic-blog/src/app/components/home/home.component.ts
+++ Epic-blog/src/app/components/home/home.component.ts
@@ -9,17 +9,24 @@ import { iJSONresponse } from '../../interfaces/i-jsonresponse';
   styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
+  arrayPost!: Post[]; // Tutti i post
+  filteredPosts: Post[] = []; // Post filtrati che saranno visualizzati
+  uniqueTags: string[] = []; // Tag unici
+  postInEvidenza!: Post; // Post in evidenza
+  arrayRandomPost: Post[] = []; // Array per i post casuali
+  showForm: boolean = false; // Flag per mostrare/nascondere il form
+  selectedTag: string | null = null; // Tag selezionato per filtrare
+
   constructor(private postSvc: PostService) {}
 
-  arrayPost!: Post[];
-  postInEvidenza!: Post;
-  arrayRandomPost: Post[] = [];
-  showForm: boolean = false;
-
   ngOnInit(): void {
+    // Ottieni tutti i post e inizializza i dati
     this.postSvc.getAllPost().then((dati: iJSONresponse) => {
-      this.arrayPost = dati.posts;
-      this.getPostRandom();
+      this.arrayPost = dati.posts; // Carica tutti i post
+      this.postSvc.setPosts(this.arrayPost); // Imposta i post nel servizio
+      this.filteredPosts = this.arrayPost; // Mostra tutti i post all'inizio
+      this.getPostRandom(); // Ottieni i post casuali
+      this.uniqueTags = this.postSvc.getUniqueTags(this.arrayPost); // Ottiene i tag unici dal service
     });
   }
 
@@ -29,6 +36,18 @@ export class HomeComponent implements OnInit {
       this.postInEvidenza = this.arrayPost[randomIndex];
       this.arrayRandomPost.push(this.arrayPost[randomIndex]);
     }
+  }
+
+  // Funzione per filtrare i post in base al tag selezionato
+  filterPostsByTag(tag: string): void {
+    this.selectedTag = tag; // Imposta il tag selezionato
+    this.filteredPosts = this.postSvc.filterPostsByTag(tag); // Filtra i post dal service
+  }
+
+  // Funzione per reimpostare i post (mostra tutti i post)
+  clearFilter(): void {
+    this.selectedTag = null; // Nessun tag selezionato
+    this.filteredPosts = this.arrayPost; // Mostra di nuovo tutti i post
   }
 
   // Funzione per aprire/chiudere il form
--- Epic-blog/src/app/service/post.service.ts
+++ Epic-blog/src/app/service/post.service.ts
@@ -1,14 +1,39 @@
 import { Injectable } from '@angular/core';
-
+import { Post } from '../interfaces/post';
 import { iJSONresponse } from '../interfaces/i-jsonresponse';
 
 @Injectable({
   providedIn: 'root',
 })
 export class PostService {
+  private posts: Post[] = []; // Array per memorizzare i post
+
   constructor() {}
 
-  getAllPost() {
-    return <Promise<iJSONresponse>>fetch('db.json').then((res) => res.json());
+  // Ottieni tutti i post da un file JSON
+  getAllPost(): Promise<iJSONresponse> {
+    return fetch('db.json').then((res) => res.json());
+  }
+
+  // Imposta i post nel servizio
+  setPosts(posts: Post[]): void {
+    this.posts = posts;
+    console.log(posts);
+  }
+
+  // Ottieni i tag unici dai post
+  getUniqueTags(posts: Post[]): string[] {
+    const tagSet = new Set<string>(); // Set per conservare tag unici
+    posts.forEach((post) => {
+      post.tags.forEach((tag) => {
+        tagSet.add(tag); // Aggiunge i tag al Set
+      });
+    });
+    return Array.from(tagSet); // Restituisce i tag unici come array
+  }
+
+  // Filtra i post in base al tag selezionato
+  filterPostsByTag(tag: string): Post[] {
+    return this.posts.filter((post) => post.tags.includes(tag)); // Filtra i post
   }
 }
